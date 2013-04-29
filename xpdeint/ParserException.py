ff6003e1843dfe42fbd816d47da8c8d872b4e87d
Jacopo <dedalusj@gmail.com>
2013-04-30 09:41:07 +1000

    Revert "Added empty line after warning messages"
    This reverts commit fc74d4df3bb0a4978317f2038d4269101dde31bb.

--- xpdeint/ParserException.py
+++ xpdeint/ParserException.py
@@ -58,6 +58,5 @@ def parserWarning(element, msg):
     warningsGiven.add((lineNumber, columnNumber, msg))
     print >> sys.stderr, indentMessageWithPrefix('WARNING: ', msg)
     print >> sys.stderr, "    At line %(lineNumber)i, column %(columnNumber)i" % locals()
-    print >> sys.stderr, ''
     # print >> sys.stderr, "    In element: " + element.userUnderstandableXPath()
 
